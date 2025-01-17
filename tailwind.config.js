432887e04d3aae0a0574a9b078a098bef6aa0b62
victorroddev <javier.rod.dev@gmail.com>
2025-01-17 14:56:11 -0700

    Test Animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,19 @@ module.exports = {
         empire: ['Roboto Slab'],
         noto: ['Noto Sans']
       },
+
+      backgroundImage: {
+        'hero-desktop': "url('/public/img/hero-desktop.png')",
+        'hero-mobile': "url('/public/img/hero-mobile-large .png')"
+      },
+
       colors: {
         primaryYellow: 'rgba(221, 143, 25)',
         primaryWhite: 'rgba(249, 249, 249)',
         primaryBlack: 'rgba(28, 28, 28)',
         secondaryBlack: 'rgba(36, 36, 36)',
         primaryGray: 'rgba(45, 45, 45)',
+        primaryGrayOpacity: 'rgba(45, 45, 45 / 75%)',
         secondaryYellow: 'rgba(157, 134, 41)'
       },
   
