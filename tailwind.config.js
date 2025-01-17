0fb1b53fff0cf06c1b9013269d666e797a2b8d91
victorroddev <javier.rod.dev@gmail.com>
2025-01-17 09:20:18 -0700

    ADD favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  theme: {    
+    extend: {
+      fontFamily: {
+        empire: ['Roboto Slab'],
+        noto: ['Noto Sans']
+      },
+      colors: {
+        primaryYellow: 'rgba(221, 143, 25)',
+        primaryWhite: 'rgba(249, 249, 249)',
+        primaryBlack: 'rgba(28, 28, 28)',
+        secondaryBlack: 'rgba(36, 36, 36)',
+        primaryGray: 'rgba(45, 45, 45)',
+        secondaryYellow: 'rgba(157, 134, 41)'
+      },
+  
+    },
   },
   plugins: [],
 }
