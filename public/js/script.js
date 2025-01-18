41568111e3ea214a2e95d2f3937c9bb287580865
victorroddev <javier.rod.dev@gmail.com>
2025-01-17 17:17:29 -0700

    Test for deploy

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.getElementById('menu-icon');
     const menu = document.getElementById('menu');
+    const navBar = document.getElementById('navbar');
+    const closeIcon = document.getElementById('close-icon');
+    const aboutIndex = document.getElementById('about-index');
+    const servicesIndex = document.getElementById('services-index');
+    const pricesIndex = document.getElementById('prices-index');
+    const apointmentsIndex = document.getElementById('apointments-index');
+    const locationIndex = document.getElementById('location-index');
+    const contactIndex = document.getElementById('contact-index');
 
     menuIcon.addEventListener('click', () => {
         menu.classList.toggle('hidden');
-        menu.classList.toggle('menu-active')
+        navBar.classList.toggle('hidden');
+    });
+
+    closeIcon.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+        navBar.classList.toggle('hidden');
+        navBar.classList.toggle('animate-fade-in'); 
+    });
+
+        
 
 });
-})
